a9c56a5a29594c01ed75d652a5d1f320f61df51c
Lars Henrik Nelson <larshnelson@protonmail.com>
2023-10-06 16:23:07 -0400

    updated toml file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,9 @@
 import datetime
 
+__version__ = datetime.datetime.now().strftime("%Y.%m.%d")
+
 from . import (
     plotting,  # noqa: F401
     stats,  # noqa: F401
 )
 from .stats import *  # noqa: F401
-
-now = datetime.datetime.now()
-
-__version__ = f"{now.year}.{now.month}.{now.day}"
